a48584c5514b73fedd37a7f02154fe0f7b9f64f8
Mateus Mayan <mateusmayan@icloud.com>
2025-03-25 19:41:52 -0300

    add exception handling with custom error responses for resource not found and database errors

---
+++ src/main/java/com/mayan/crud_clientes/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,63 @@
+package com.mayan.crud_clientes.controllers.handlers;
+
+import com.mayan.crud_clientes.dtos.CustomError;
+import com.mayan.crud_clientes.dtos.ValidationError;
+import com.mayan.crud_clientes.services.exceptions.DatabaseException;
+import com.mayan.crud_clientes.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        CustomError err = new CustomError(
+                Instant.now(),
+                status.value(),
+                e.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        ValidationError err = new ValidationError(
+          Instant.now(),
+          status.value(),
+          e.getMessage(),
+          request.getRequestURI()
+        );
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        ValidationError err = new ValidationError(
+          Instant.now(),
+          status.value(),
+          "Dados inválidos",
+          request.getRequestURI()
+        );
+
+        e.getBindingResult().getFieldErrors().forEach(f -> {
+            err.addError(f.getField(), f.getDefaultMessage());
+        });
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
